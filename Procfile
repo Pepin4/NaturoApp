742b5e091685060d5de03a1e55452d84100705cc
pepin <martel.dimi@gmail.com>
2025-05-31 18:56:16 +0200

    MAJ 5

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT -jar target/NaturoApp-0.0.1-SNAPSHOT.jar
+web: java -jar target/NaturoApp-0.0.1-SNAPSHOT.jar
--- src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
+++ src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
@@ -7,19 +7,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NaturopathieApplication  {
+public class NaturopathieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NaturopathieApplication .class, args);
-		SpringApplication app = new SpringApplication(NaturopathieApplication.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(NaturopathieApplication.class);
         Map<String, Object> props = new HashMap<>();
+
         String port = System.getenv("PORT");
+        System.out.println("✅ PORT ENV: " + port); // debug log
+
         if (port != null) {
             props.put("server.port", port);
+        } else {
+            props.put("server.port", 8080); // fallback
         }
+
         app.setDefaultProperties(props);
         app.run(args);
-	}
-
-	
+    }
 }
