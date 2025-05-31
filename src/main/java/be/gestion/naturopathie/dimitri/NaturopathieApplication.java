28a753e4a515930e64fd78430374c7681030ff13
pepin <martel.dimi@gmail.com>
2025-06-01 01:25:41 +0200

    MAJ 6

--- src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
+++ src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
@@ -1,28 +1,14 @@
 package be.gestion.naturopathie.dimitri;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NaturopathieApplication {
+public class NaturopathieApplication  {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(NaturopathieApplication.class);
-        Map<String, Object> props = new HashMap<>();
+	public static void main(String[] args) {
+		SpringApplication.run(NaturopathieApplication .class, args);
+	}
 
-        String port = System.getenv("PORT");
-        System.out.println("✅ PORT ENV: " + port); // debug log
-
-        if (port != null) {
-            props.put("server.port", port);
-        } else {
-            props.put("server.port", 8080); // fallback
-        }
-
-        app.setDefaultProperties(props);
-        app.run(args);
-    }
+	
 }
