b55e5bb75f9f0066f12788f458c4d2afeafba37c
pepin <martel.dimi@gmail.com>
2025-06-01 02:25:46 +0200

    MAJ main

--- src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
+++ src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
@@ -3,12 +3,20 @@ package be.gestion.naturopathie.dimitri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
-public class NaturopathieApplication  {
+public class NaturopathieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NaturopathieApplication .class, args);
-	}
+    public static void main(String[] args) {
+        // Récupère le port depuis la variable d'environnement, ou 8080 par défaut
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
 
-	
+        SpringApplication app = new SpringApplication(NaturopathieApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        app.run(args);
+    }
 }
