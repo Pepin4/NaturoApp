e60f8868882870dbc942d1ff02d96ff6785eea93
pepin <martel.dimi@gmail.com>
2025-06-01 11:54:02 +0200

    Procfile

--- src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
+++ src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
@@ -2,18 +2,13 @@ package be.gestion.naturopathie.dimitri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class NaturopathieApplication {
+public class NaturopathieApplication  {
 
-    public static void main(String[] args) {
-        // Démarrage de l'application et récupération du contexte
-        ConfigurableApplicationContext context = SpringApplication.run(NaturopathieApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(NaturopathieApplication .class, args);
+	}
 
-        // Récupération de l'environnement pour afficher le port utilisé
-        Environment env = context.getEnvironment();
-        System.out.println("Application started on port: " + env.getProperty("local.server.port"));
-    }
+	
 }
