3b973883647a65bddd285cd0a39c6f996a38d848
pepin <martel.dimi@gmail.com>
2025-06-01 03:46:08 +0200

    MAJ6

--- src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
+++ src/main/java/be/gestion/naturopathie/dimitri/NaturopathieApplication.java
@@ -1,15 +1,16 @@
 package be.gestion.naturopathie.dimitri;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class NaturopathieApplication implements CommandLineRunner {
 
-    @Value("${server.port}")
-    private String serverPort;
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(NaturopathieApplication.class, args);
@@ -17,6 +18,7 @@ public class NaturopathieApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Server is running on port: " + serverPort);
+        String port = env.getProperty("local.server.port");
+        System.out.println("Server is running on port: " + port);
     }
 }
